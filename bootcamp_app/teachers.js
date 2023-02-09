5fb04a5be952e3f92ea6769d9ce6218e358c9a95
fayesadev <fayedumbrigue@gmail.com>
2023-02-09 06:42:52 +0000

    Implement teachers connection to the server from database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,14 +14,16 @@ pool.connect().then(()=> {
 })
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${input[0]}%'
-LIMIT ${input[1] || 5};
+ORDER BY teachers.name;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
